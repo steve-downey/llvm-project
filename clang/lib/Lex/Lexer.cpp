36e31ef9cdd41b348a0220cba5b35388759c5502
Steve Downey <sdowney@gmail.com>
2026-06-26 18:47:20 -0400

    [backtick] S02: Lexer: backtick punctuator token
    Add PUNCTUATOR(backtick, "`") to TokenKinds.def and a case '`': in
    LexTokenInternal that mints tok::backtick when LangOpts.Backtick is on
    and tok::unknown otherwise. String literals, raw-string literals, and
    comments are unaffected (consumed before the punctuator switch).
    Lit test clang/test/Lexer/backtick-token.cpp covers all three paths.

--- clang/lib/Lex/Lexer.cpp
+++ clang/lib/Lex/Lexer.cpp
@@ -4529,6 +4529,10 @@ LexStart:
       Kind = tok::unknown;
     break;
 
+  case '`':
+    Kind = LangOpts.Backtick ? tok::backtick : tok::unknown;
+    break;
+
   // UCNs (C99 6.4.3, C++11 [lex.charset]p2)
   case '\\':
     if (!LangOpts.AsmPreprocessor) {
