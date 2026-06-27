bef75437877e027a260ff1825fb455eee5a05ef3
Steve Downey <sdowney@gmail.com>
2026-06-27 12:31:43 -0400

    [backtick] S07: Keyword-escape identifiers (position-based)

--- clang/lib/Parse/ParseDecl.cpp
+++ clang/lib/Parse/ParseDecl.cpp
@@ -6770,7 +6770,8 @@ void Parser::ParseDirectDeclarator(Declarator &D) {
     }
 
     if (Tok.isOneOf(tok::identifier, tok::kw_operator, tok::annot_template_id,
-                    tok::tilde)) {
+                    tok::tilde) ||
+        (getLangOpts().Backtick && Tok.is(tok::backtick))) {
       // We found something that indicates the start of an unqualified-id.
       // Parse that unqualified-id.
       bool AllowConstructorName;
